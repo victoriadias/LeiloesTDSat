1370ec4054e23eb8651898ed312d890029c78672
Victoria Dias <vick.diasx@hotmail.com>
2026-01-12 05:51:00 -0300

    Implementado funcionalidade de salvar produto no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,12 +23,19 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    conn = new conectaDAO().connectDB();
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
+    } catch (Exception e) {
+        System.out.println("Erro ao cadastrar: " + e.getMessage());
     }
+}
+    
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
